eee9c02d85752dbe62b366c0de184e291875c47a
bitbonk <bitbonk@msn.com>
2020-04-22 16:21:46 +0200

    Initial commit

---
+++ RiderExceptionDebuggingBug/UnitTest1.cs
@@ -0,0 +1,46 @@
+using System;
+using Xunit;
+
+namespace RiderExceptionDebuggingBug
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class UnitTest1
+    {
+        [Fact]
+        public async Task StartKernelConfigurationMessage_Kernel_Described()
+        {
+            var cancellationTokenSource = new CancellationTokenSource();
+            var cancellationToken = cancellationTokenSource.Token;
+            
+            var anyTask = DoSomethingAsync().CancelWithCancellationToken(cancellationToken);
+            
+
+            await Task.Delay(1000);
+            
+            
+            
+            cancellationTokenSource.Cancel();
+
+            var result = await anyTask;
+
+            async Task<string> DoSomethingAsync()
+            {
+                await Task.Delay(3000);
+                return "done!";
+            }
+        }
+    }
+    
+    public static class TaskExtensionMethods
+    {
+        public static async Task<TResult> CancelWithCancellationToken<TResult>(this Task<TResult> task, CancellationToken cancellationToken)
+        {
+            var taskCompletionSource = new TaskCompletionSource<TResult>();
+            cancellationToken.Register(() => taskCompletionSource.TrySetCanceled());
+            var anyTask = await Task.WhenAny(task, taskCompletionSource.Task);
+            return await anyTask;
+        }
+    }
+}
